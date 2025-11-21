85ca8eed4b9939c310c42098aa58b6a0ad311fe0
Ahmed ali <sertyd37@gmail.com>
2025-11-21 15:57:49 -0300

    modify movie/admin.py by add favoriteList model to admin site register

--- movie/admin.py
+++ movie/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import movie
+from .models import movie,favoriteList
 # Register your models here.
-admin.site.register(movie)
+admin.site.register((movie,favoriteList))
