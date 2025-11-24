bd581c1a2e2ab901bc2e06a2ce4e66777cd5b202
Ahmed ali <sertyd37@gmail.com>
2025-11-24 11:50:18 -0300

    modify settings for deploying

--- dbmi/settings.py
+++ dbmi/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e&p(a*x&g(+l$t04!^98km2_816@gj^s#wtw+2eno4zi@nzdax'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') or 'bc(l*j%@-+2on@#%9bnpnq3(p%zr^kzcf_*0ug_^5&j218xiaj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = str(os.environ.get('DEBUG'))=='1'
+ENV_ALLOWED_HOST=os.environ.get('DJANGO_ALLOWED_HOST') or None
 ALLOWED_HOSTS = []
+if ENV_ALLOWED_HOST is not None :
+    ALLOWED_HOSTS=[ENV_ALLOWED_HOST]
+
 
 # print(BASE_DIR)
 # Application definition
@@ -74,6 +77,16 @@ WSGI_APPLICATION = 'dbmi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+POSTGRES_DB=os.environ.get('POSTGRES_DB')
+POSTGRES_PASSWORD=os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_USER=os.environ.get('POSTGRES_USER')
+POSTGRES_HOST=os.environ.get('POSTGRES_HOST')
+POSTGRES_PORT=os.environ.get('POSTGRES_PORT')
+POSTGRES_READY=(POSTGRES_DB is not None 
+                and POSTGRES_PASSWORD is not None
+                and POSTGRES_USER is not None 
+                and POSTGRES_HOST is not None and POSTGRES_PORT is not None)
+
 
 DATABASES = {
     'default': {
@@ -81,6 +94,22 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+if POSTGRES_READY:
+    DATABASES ={
+        'default':{
+            'ENGINE':'django.db.backends.postgresql',
+            'NAME':POSTGRES_DB,
+            'USER':POSTGRES_USER,
+            'PASSWORD':POSTGRES_PASSWORD,
+            'HOST':POSTGRES_HOST,
+            'PORT':POSTGRES_PORT
+
+        }
+    }
+
+
+
 
 
 # Password validation
