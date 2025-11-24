fe5ff02d11a6aeec9baee97738a00982fbf42211
Ahmed ali <sertyd37@gmail.com>
2025-11-24 13:42:54 -0300

    modify manage.py so it check dot env file existence

--- manage.py
+++ manage.py
@@ -4,13 +4,18 @@ import os
 import sys
 import dotenv
 import logging
+import pathlib
 logging.basicConfig(level=logging.DEBUG,format=' %(filename)s- %(asctime)s - %(levelname)s - %(message)s')
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dbmi.settings')
     try:
-        dotenv.read_dotenv()
+        dotenv_File_path=pathlib.Path() / '.env'
+        if dotenv_File_path.exists() :
+         dotenv.read_dotenv()
+        else:
+          raise FileNotFoundError("i can't finde dot env file !!")
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
